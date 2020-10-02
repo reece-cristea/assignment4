2cb5f8bd6719d60d8fe3f885fa8d62cb31b14ea6
reece cristea <reece.cristea@mymail.champlain.edu>
2020-10-02 02:19:37 -0700

    Addition of bubble, selection and insertion sort should be everything sorry it was a little late.

--- src/sort.h
+++ src/sort.h
@@ -38,21 +38,74 @@ namespace csi281 {
     // using the bubble sort algorithm
     template <typename T>
     void bubbleSort(T array[], const int length) {
-        // YOUR CODE HERE
+		//init temp variable to swap variables in the array
+		T temp;
+		//start first cycle through array
+		for (int i = 0; i < (length - 1); i++)
+		{
+			//go through each element in array
+			for (int j = 0; j < (length - i - 1); j++)
+			{
+				//compare if element is bigger than the one after it, if so swap them to sort
+				if (array[j] > array[j + 1])
+				{
+					temp = array[j];
+					array[j] = array[j + 1];
+					array[j + 1] = temp;
+				}
+			}
+		}
     }
     
     // Performs an in-place ascending sort of *array* of size *length*
     // using the selection sort algorithm
     template <typename T>
     void selectionSort(T array[], const int length) {
-        // YOUR CODE HERE
+		//initiate temp for swapping and the smallest elements index
+		T temp;
+		int minIndex;
+		//start 1st cycle through array
+		for (int i = 0; i < (length - 1); i++)
+		{
+			//set minIndex to first index
+			minIndex = i;
+			//compare elements
+			for (int j = i + 1; j < length; j++)
+			{
+				//if an element is smaller than the minimum make the element the new minimum
+				if (array[j] < array[minIndex])
+				{
+					minIndex = j;
+					temp = array[minIndex];
+					array[minIndex] = array[i];
+					array[i] = temp;
+				}
+			}
+		}
     }
     
     // Performs an in-place ascending sort of *array* of size *length*
     // using the insertion sort algorithm
     template <typename T>
     void insertionSort(T array[], const int length) {
-        // YOUR CODE HERE
+		//initiate key element and j to compare key to its predcessors
+		T key;
+		int j;
+		//start iteration through array
+		for (int i = 1; i < length; i++)
+		{
+			//set the key element to the element proceeding the one at hand
+			key = array[i];
+			j = i - 1;
+			//compare key element to its predecessors and put key in its sorted place
+			while (j >= 0 && array[j] > key)
+			{
+				array[j + 1] = array[j];
+				j -= 1;
+			}
+			//set next element to key
+			array[j + 1] = key;
+		}
     }
 }
 
